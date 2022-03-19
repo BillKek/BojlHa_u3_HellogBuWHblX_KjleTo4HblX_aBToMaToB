ab76d23c88846e9b03dbac1f89c0ebb13952b603
BillKek <51540758+BillKek@users.noreply.github.com>
2022-03-19 17:00:03 +0300

    более совместимо с вебом

--- Win32/Debug/convert.bat
+++ Win32/Debug/convert.bat
@@ -1,5 +1,5 @@
 SET PATH=E:\ffmpeg-4.2.2-win64-static\bin
-rem ffmpeg -encoders
+rem ffmpeg -encoders > list.txt
 rem pause
-ffmpeg -i Project1.exe%%d.bmp -r 10 -c:v libx265 -preset slow output.mp4
+ffmpeg -i Project1.exe%%d.bmp -r 10 -c:v libx264 -profile:v main -level "3.0" -an -s 320x180 -pix_fmt yuv420p output.mp4
 pause
